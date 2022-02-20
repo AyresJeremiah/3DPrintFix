7b80a04f46dc69ae075e65a3310a87af4faac520
Jeremiah J Ayres <ayresjeremiah@gmail.com>
2022-02-19 20:56:25 -0500

    Add files via upload

--- Fix3DPrint.java
+++ Fix3DPrint.java
@@ -4,102 +4,53 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 
 /**
- * Simple HelloWorld program (clear of Checkstyle and FindBugs warnings).
+ * Program to fix Gcode after a 3D print failure. USER MUST KNOW THE X, Y, Z
+ * COORDINATES AT TIME OF FAILURE.
  *
  * @author Jeremiah Ayres
  */
+
 public final class Fix3DPrint {
 
-    public static String parseXYZ(String in, double x, double y, double z) {
-        String temp = in;
-        double currentX, currentY, currentZ;
-
-        int inLength = in.length();
-
-        //Change X
-        if (in.contains("X")) {
-            int index = in.indexOf("X");
-
-            if (index + 1 < inLength) {
-                if (Character.isDigit(in.charAt(index + 1))) {
-                    String temp2 = in.substring(index + 1);
-                    String temp3 = in.substring(0, index + 1);
-                    int indexEnd = temp2.indexOf(" ");
-                    if (indexEnd == -1) {
-                        //nothing after the integer Value
-                        currentX = Double.parseDouble(temp2);
-                    } else {
-                        currentX = Double
-                                .parseDouble(temp2.substring(0, indexEnd));
-                        temp2 = temp2.substring(indexEnd);
-                    }
-                    x = x - currentX;
-                    x = ((double) ((int) (x * 1000))) / 1000;
-                    if (indexEnd == -1) {
-                        temp = temp3 + x;
-                    } else {
-                        temp = temp3 + x + temp2;
-                    }
-                }
-            }
-        }
-
-        //Change Y
-        if (in.contains("Y")) {
-            int index = in.indexOf("Y");
+    /*
+     * This function gets the last extrude command value from "in"
+     *
+     * @pram in = Array of string containing the last 100 lines of Gcode. i =
+     * current line in the in array.
+     *
+     * @return e = The E value from the Gcodem
+     */
+    public static double getE(String[] in, int i) {
+        int inLength = in[i].length();
+        double e = -1;
+        if (in[i].contains("E")) {
+            int index = in[i].indexOf("E");
 
             if (index + 1 < inLength) {
-                if (Character.isDigit(in.charAt(index + 1))) {
-                    String temp2 = in.substring(index + 1);
-                    String temp3 = in.substring(0, index + 1);
+                if (Character.isDigit(in[i].charAt(index + 1))) {
+                    String temp2 = in[i].substring(index + 1);
                     int indexEnd = temp2.indexOf(" ");
+
                     if (indexEnd == -1) {
                         //nothing after the integer Value
-                        currentY = Double.parseDouble(temp2);
+                        e = Double.parseDouble(temp2);
                     } else {
-                        currentY = Double
-                                .parseDouble(temp2.substring(0, indexEnd));
+                        e = Double.parseDouble(temp2.substring(0, indexEnd));
                         temp2 = temp2.substring(indexEnd);
                     }
-                    y = y - currentY;
-                    y = ((double) ((int) (y * 1000))) / 1000;
-                    if (indexEnd == -1) {
-                        temp = temp3 + y;
-                    } else {
-                        temp = temp3 + y + temp2;
-                    }
+
                 }
             }
-        }
-        //Change Z
-        if (in.contains("Z")) {
-            int index = in.indexOf("Z");
-
-            if (index + 1 < inLength) {
-                if (Character.isDigit(in.charAt(index + 1))) {
-                    String temp2 = in.substring(index + 1);
-                    String temp3 = in.substring(0, index + 1);
-                    int indexEnd = temp2.indexOf(" ");
-                    if (indexEnd == -1) {
-                        //nothing after the integer Value
-                        currentZ = Double.parseDouble(temp2);
-                    } else {
-                        currentZ = Double
-                                .parseDouble(temp2.substring(0, indexEnd));
-                        temp2 = temp2.substring(indexEnd);
-                    }
-                    z = z - currentZ;
-                    z = ((double) ((int) (z * 1000))) / 1000;
-                    if (indexEnd == -1) {
-                        temp = temp3 + z;
-                    } else {
-                        temp = temp3 + z + temp2;
-                    }
-                }
+            //E Doesn't Exist in the current index string
+        } else {
+            if (i == 0) {
+                i = 100;
             }
+            e = getE(in, i--);
         }
 
-        return temp;
+        return e;
+
     }
 
     /**
@@ -107,13 +58,13 @@ public final class Fix3DPrint {
      *
      * @param args
      *            the command line arguments; unused here
-     * @throws FileNotFoundException
+     *
      */
     public static void main(String[] args) throws FileNotFoundException {
         String x, y, z, line, fileName = ".gcode";
-        double xDouble = 96, yDouble = 107.1, zDouble = 23;
-        boolean powercycle = true;
-        int numAstrix = 0;
+        String[] previousLines = new String[100];
+        double e;
+        int i = 0;
 
         Scanner usrIn = new Scanner(System.in);
         PrintWriter out = new PrintWriter("data/output.gcode");
@@ -128,68 +79,53 @@ public final class Fix3DPrint {
         Scanner in = new Scanner(input);
 
         System.out.println("Enter X: ");
-        x = usrIn.nextLine();
-        xDouble = Double.parseDouble(x);
-        x = "X" + x;
+        x = "X" + usrIn.nextLine();
         System.out.println(x);
 
         System.out.println("Enter Y: ");
-        y = usrIn.nextLine();
-        yDouble = Double.parseDouble(y);
-        y = "Y" + y;
+        y = "Y" + usrIn.nextLine();
+
         System.out.println(y);
 
         System.out.println("Enter Z: ");
-        z = usrIn.nextLine();
-        zDouble = Double.parseDouble(z);
-        z = "Z" + z;
+        z = "Z" + usrIn.nextLine();
         System.out.println(z);
-
-        System.out.println("Did the printer have a power cycle? y/n");
-        powercycle = usrIn.nextLine().contains("y");
 
         System.out.println("Running!");
 
-        if (!powercycle) {
-            while (in.hasNext()) {
-                line = in.nextLine();
-                if (line.contains(z)) {
-                    while (in.hasNext()) {
-                        if (line.contains(x) && line.contains(y)) {
+        while (in.hasNext()) {
+            line = in.nextLine();
+            if (line.contains(z)) {
+                while (in.hasNext()) {
+
+                    //Rotate i back to zero if it is at the end of the array
+                    if (i == 100) {
+                        i = 0;
+                    }
+                    //Save previous lines.
+                    previousLines[i] = line;
+                    i++;
+
+                    if (line.contains(x) && line.contains(y)) {
+
+                        //Print the current position to the output file;
+                        e = getE(previousLines, i - 1);
+                        //e should be > 0 at this point.
+                        out.println("G92 " + x + " " + y + " " + z + " E" + e);
+
+                        line = in.nextLine();
+                        out.println(line);
+                        while (in.hasNext()) {
                             line = in.nextLine();
                             out.println(line);
-                            while (in.hasNext()) {
-                                line = in.nextLine();
-                                out.println(line);
-                            }
-                        } else {
-                            line = in.nextLine();
                         }
-                    }
-                }
-            }
-        } else {
-            while (in.hasNext()) {
-                line = in.nextLine();
-                if (line.contains(z)) {
-                    while (in.hasNext()) {
-                        if (line.contains(x) && line.contains(y)) {
-                            line = in.nextLine();
-
-                            out.println(
-                                    parseXYZ(line, xDouble, yDouble, zDouble));
-                            while (in.hasNext()) {
-                                line = in.nextLine();
-                                out.println(parseXYZ(line, xDouble, yDouble,
-                                        zDouble));
-                            }
-                        } else {
-                            line = in.nextLine();
-                        }
+                    } else { //X and Y not fund yet
+                        line = in.nextLine();
                     }
                 }
             }
         }
+
         System.out.println();
         System.out.println("COMPLETE!");
         usrIn.close();
